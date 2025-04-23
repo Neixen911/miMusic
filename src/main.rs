a6c1cf7905438f8abb2b5c6bcfc8e39385855aeb
neixen911 <julien.quelin33@gmail.com>
2025-04-23 10:15:41 +0200

    Add fonctionnal play, pause and exit command + display infos about playing song

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
+use std::io;
+use std::io::BufReader;
+use std::fs::File;
+use std::collections::HashMap;
+use rodio::{Decoder, OutputStream, Sink};
+use id3::{Tag, Content};
+
+struct Player {
+    m_song_infos: HashMap<String, String>,
+}
+
+fn d_playing_infos(sink: &Sink, player: &Player) {
+    if sink.empty() {
+        println!("No song is currently playing.");
+    } else {
+	if !player.m_song_infos.is_empty() {
+	    for (key, value) in &player.m_song_infos {
+	   	println!("{}: {}", key, value);
+	    }
+	}
+	println!("Position of song: {}s", &sink.get_pos().as_secs());
+	println!("Volume: {}", sink.volume());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut player = Player { m_song_infos: HashMap::new() };
+    let (_stream, handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&handle).unwrap();
+
+    loop {
+        println!("Enter a command !");
+        let mut input = String::new();
+        io::stdin()
+                .read_line(&mut input)
+                .unwrap();
+
+        let input = input.as_str();
+
+        match input {
+            "pause\n" => {
+                sink.pause();
+                d_playing_infos(&sink, &player);
+            },
+
+            "play\n" => {
+                if sink.empty() {
+                    let file = File::open("song.mp3").unwrap();
+		    let tag = Tag::read_from2(&file).unwrap();
+		    
+		    for frame in tag.frames() {
+			let id = frame.id();
+			
+			match frame.content() {
+			    Content::Text(value) => {
+				
+				match id {
+				    "TIT2" => {
+					player.m_song_infos.insert(String::from("title"), value.to_string());
+				    }
+
+				    "TPE1" => {
+					player.m_song_infos.insert(String::from("artist"), value.to_string());
+				    }
+
+				    _default => {
+					continue;
+				    }
+				}
+
+			    }
+
+			    _content => {
+				continue;
+			    }
+			}
+		    }
+
+                    let buffer = BufReader::new(file);
+		    sink.append(Decoder::new_mp3(buffer).unwrap());
+		    d_playing_infos(&sink, &player);
+                } else {
+                    sink.play();
+		    d_playing_infos(&sink, &player);
+                }
+            },
+
+            "exit\n" => {
+                break;
+            },
+
+            _ => todo!(),
+        }
+    }
 }
