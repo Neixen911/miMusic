805285470e761c1dc00d609c94332e076fd07640
neixen911 <julien.quelin33@gmail.com>
2025-07-09 13:36:12 +0200

    feat: download songs from playlist URL

--- src/bin/ytdlp.rs
+++ src/bin/ytdlp.rs
@@ -1,41 +1,61 @@
 use std::process::Command;
 use std::path::PathBuf;
 use std::io;
+use std::fs::File;
+use std::io::Write;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rand::prelude::IndexedRandom;
 use walkdir::WalkDir;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     // Setup the librairies
     let libraries_dir = PathBuf::from("libs");
     let yt_dlp = libraries_dir.join("yt-dlp");
+    let url = "https://www.youtube.com/playlist?list=PLOzDu-MXXLlg5384VEAWMzgXSdU8HFwbS";
 
-    // Select a random genre
-    let genres = vec!["lofi", "pop"];
-    let mut rng = thread_rng();
-    let random_genre = genres.choose(&mut rng);
+    println!("Starting fetching songs from playlist ...");
 
-    // Set the name of the output file
-    let output_dir = PathBuf::from("songs");
-    let nb_song = WalkDir::new(&output_dir).into_iter().count() - 1;
-    let output_file = output_dir.join("song".to_owned() + &nb_song.to_string() + ".%(ext)s");
-
-    let search_base_prompt = "Best ".to_owned() + random_genre.unwrap() + " songs playlist";
-
-    let url = "https://youtube.com/watch?v=kPa7bsKwL-c&list=PLDIoUOhQQPlXqz5QZ3dx-lh_p6RcPeKjv";
-
+    // Fetching songs URL from playlist
     let mut binding = Command::new(yt_dlp.to_str().unwrap());
     let status = binding.args([
-        "--no-write-subs", 
-        "-x", 
-        "--audio-format", "mp3", 
-        "--no-playlist", 
-        "--add-metadata", 
-        "-o", output_file.to_str().unwrap(), 
+        "--skip-download", 
+        "--print", "%(webpage_url)s", 
         url, 
-    ]).status()?;
+    ]).output().unwrap();
+
+    println!("Done !");
+
+    // String output to vec of urls
+    let json_data = String::from_utf8_lossy(&status.stdout);
+    let urls: Vec<String> = json_data
+        .lines()
+        .map(|line| line.trim().to_string())
+        .collect();
+
+    println!("Starting to download songs ...");
+
+    // Download songs from playlist
+    let output_dir = PathBuf::from("songs");
+    let mut id_song = WalkDir::new(&output_dir).into_iter().count() - 1;
+    for song_url in urls {
+        let filename = output_dir.join("song".to_owned() + &id_song.to_string() + ".%(ext)s");
+        let mut binding = Command::new(yt_dlp.to_str().unwrap());
+        let status = binding.args([
+            "--no-write-subs", 
+            "-x", 
+            "--audio-format", "mp3", 
+            "--add-metadata", 
+            "-o", filename.to_str().unwrap(), 
+            &song_url, 
+        ]).status()?;
+        println!("Song {} downloaded !", song_url);
+        id_song = id_song + 1;
+    }
+
+    println!("Done !");
 
     Ok(())
 }
